03d195eebd492eec555b466c149449586659267f
Niken Alfrido <alfrisiboro@gmail.com>
2024-07-30 03:27:24 +0700

    add express

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Test!')
+})
+
+app.listen(port, () => {
+    console.log(`Server berjalan di port ${port}`);
+})
+
+console.log("Kiwkiw");
